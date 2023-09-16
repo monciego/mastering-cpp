75a98f704c7de57388d814e6a8fdfc04cdde6328
Jericho <jerichobantiquete@gmail.com>
2023-09-16 10:03:21 +0800

    comments

---
+++ 1-basic/1.2-comments/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // include iostream library
+
+int main() { // entry point of the program
+    // print hello world
+    std::cout << "Hello World" << std::endl;
+
+    /* 
+        this is
+        multiline
+        comment
+     */
+
+    return 0;
+    // program ends
+}
